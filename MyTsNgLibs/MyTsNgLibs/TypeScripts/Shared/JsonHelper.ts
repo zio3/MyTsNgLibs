1d2b9750fb5810f3ba318cc30d8ef9765ecf9f74
zio3 <info@zio3.net>
2015-02-22 22:03:04 +0900

    ファイル追加

---
+++ MyTsNgLibs/MyTsNgLibs/TypeScripts/Shared/JsonHelper.ts
@@ -0,0 +1,28 @@
+﻿module JsonHelper {
+    export function toDebugJson(src:any,...ignoreFields: string[]):string {
+        var items = _.pairs(src);
+        var obj = new Object();
+        _(items).filter(i=> i[0].indexOf("$") != 0) //フレームワークのオブジェクトを除外
+            .filter(i=> ignoreFields.indexOf(i[0]) == -1) //指定した名前を除外しておく
+            .filter(i=> !_.isFunction(i[1]))//関数を除外
+            .filter(i=> i[1] != src)　　　 //自身のオブジェクトを除外(無限ループ対策)
+            .forEach(i=> obj[i[0]] = JsonHelper.objectFilter(i[1]));
+
+        var str = JSON.stringify(obj, null, "    ");
+        return str;
+    }
+
+    export function objectFilter(src: any):any {
+        var items = _.pairs(src);
+
+        if (items.length == 0)
+            return src;
+
+        var dst = new Object();
+        _(items).filter(i=> i[0].indexOf("$") != 0) //フレームワークのオブジェクトを除外
+            .filter(i=> !_.isFunction(i[1]))//関数を除外
+            .filter(i=> i[1] != src)　　　 //自身のオブジェクトを除外(無限ループ対策)
+            .forEach(i=> dst[i[0]] = JsonHelper.objectFilter(i[1]));
+        return dst;
+    }
+} 
---
+++ MyTsNgLibs/MyTsNgLibs/TypeScripts/Shared/PaginationList.ts
@@ -0,0 +1,52 @@
+﻿/// <reference path="../../Scripts/typings/underscore/underscore.d.ts" />
+
+interface pageNavi {
+    text: string;
+    index: number;
+    state: string;
+}
+
+class PaginationList<T> {
+    CurrentItems = new Array<T>();
+    pageIndex = 0;
+    pageSize = 10;
+    totalCount = 0;
+    totalPages = 0;
+    maxNaviCount = 10;
+    PageNavis: pageNavi[];
+
+    constructor(pageSize: number = 10) {
+        this.pageSize = pageSize;
+    }
+
+    private genPageNavis(): pageNavi[] {
+        var pageNavis = new Array<pageNavi>();
+
+        pageNavis.push({ text: "<<", index: 0, state: (this.pageIndex != 0) ? "" : "disabled" });
+        pageNavis.push({ text: "<", index: this.pageIndex - 1, state: (this.pageIndex != 0) ? "" : "disabled" });
+
+        var naviCount = Math.min(this.totalPages, this.maxNaviCount);
+
+        var naviStartIndex = Math.max(0, Math.min(this.totalPages - naviCount, this.pageIndex - naviCount / 2));
+        var naviLastIndex = Math.min(this.totalPages, Math.max(naviCount, this.pageIndex + naviCount / 2));
+        var naviNumbers = _.range(naviStartIndex, naviLastIndex);
+
+        _.each(naviNumbers,
+            (idx: number) => {
+                pageNavis.push({ text: (idx + 1).toString(), index: idx, state: (this.pageIndex != idx) ? "" : "active" });
+            });
+
+        pageNavis.push({ text: ">", index: this.pageIndex + 1, state: (this.pageIndex != (this.totalPages - 1)) ? "" : "disabled" });
+        pageNavis.push({ text: ">>", index: this.totalPages - 1, state: (this.pageIndex != (this.totalPages - 1)) ? "" : "disabled" });
+        pageNavis.push({ text: this.totalCount + "件", index: 0, state: "disabled" });
+        return pageNavis;
+    }    
+
+    public ReplaceCurrentItem(items: T[], pageIndex: number, totalCount: number) {
+        this.pageIndex = pageIndex;
+        this.totalCount = Number(totalCount);
+        this.totalPages = Math.ceil(Number(totalCount) / this.pageSize);
+        this.CurrentItems = items;
+        this.PageNavis = this.genPageNavis();
+    }
+} 
